1f2eba62597887f969d0e4e15c2cab722a532105
rajan_ranjan <kumarkim@gmail.com>
2024-01-16 13:55:04 +0530

    ignoring some unwanted files

--- cypress/JSConcetpts/Destructuring/Project.js
+++
@@ -1,8 +0,0 @@
-//desturcturing the student object from Student
-const student = require("./Student")
-require("dotenv").config()
-
-const { name, age, discipline } = require("./Student")
-console.log(student)
-console.log(name)
-console.log(process.env.LOGIN_ORANGE_HRM)
